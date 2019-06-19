dabde55c86831e5a05d104e336399f1becde8a91
Dan Bryan <dbryan@ai-labs.cloud>
2019-06-19 09:58:44 -0400

    disable some servcies

--- content/resume/build.sh
+++ content/resume/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if ! command -v /usr/local/go/bin/go ; then
+    wget https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb
+    sudo dpkg -i pandoc-2.7.2-1-amd64.deb 
+fi
 make
 AWS_PROFILE=BRYANLABS aws s3 sync output/ s3://bryanlabs-public/resume/ --exclude "*.log" --acl public-read
